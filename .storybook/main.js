03ecb6ecd182bdbca0a9f84da4b6075156c47aee
oleg proskurin <regx@usul.su>
2021-03-14 01:04:50 +0300

    Add more addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['./stories.js'],
-  addons: ["../preset.js", '@storybook/addon-actions', '@storybook/addon-links']
+  addons: ["../preset.js", '@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-backgrounds', 'storybook-dark-mode']
 }
